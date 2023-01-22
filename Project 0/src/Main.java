1f7d86bec7c6f712112106b8a6b599191ceef85c
sufia1117 <sufia.khan1@marist.edu>
2023-01-22 14:08:38 -0500

    Project 0 with code prompting user input

--- Project 0/src/Main.java
+++ Project 0/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner; // Import Scanner class
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a Scanner object
+        Scanner input1 = new Scanner(System.in);
+
+        // Prompt user to enter driving distance
+        System.out.print("Enter the driving distance: ");
+        double distance = input1.nextDouble();
+
+        // Create a new Scanner object
+        Scanner input2 = new Scanner(System.in);
+
+        // Prompt user to enter miles per gallon
+        System.out.print("Enter the miles per gallon: ");
+        double mpg = input2.nextDouble();
+
+        // Create a new Scanner object
+        Scanner input3 = new Scanner(System.in);
+
+        // Prompt user to enter price per gallon
+        System.out.print("Enter the price per gallon: ");
+        double ppg = input3.nextDouble();
+
     }
 }
